b8143f81c4d50d24653566bba555fdafe0c07f31
Y.Wang <shengguanglong01@sina.com>
2018-11-25 14:19:38 +0800

    Version 1.0

--- Browser.py
+++ Browser.py
@@ -1,17 +1,17 @@
 from PyQt5.QtCore import QUrl, QSize
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtWidgets import QMainWindow, QToolBar, QAction, QLineEdit, QApplication, QProgressBar
+from PyQt5.QtWidgets import QMainWindow, QToolBar, QAction, QLineEdit, QProgressBar
 
 
 class BrowserWindow(QMainWindow):
-    name = "基于Qt5的 Python 浏览器 "
-    version = "0.9 Beta"
+    name = "PyBrowser Powered by Y.Wang"
+    version = "1.0.20181125"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # 窗体
-        self.setWindowTitle(self.name + self.version)
+        self.setWindowTitle(self.name)
         self.setWindowIcon(QIcon('Assets/main.png'))
         self.resize(1200, 800)
         # 浏览器窗体
@@ -21,32 +21,37 @@ class BrowserWindow(QMainWindow):
         # 工具条
         self.navigation_bar = QToolBar('Navigation')
         self.navigation_bar.setIconSize(QSize(32, 32))
+        self.navigation_bar.setMovable(False)
         self.addToolBar(self.navigation_bar)
-        # 后退前进停止刷新主页转到按钮
+        # 后退前进停止刷新按钮
         self.back_button = QAction(QIcon('Assets/back.png'), '后退', self)
         self.next_button = QAction(QIcon('Assets/forward.png'), '前进', self)
         self.stop_button = QAction(QIcon('Assets/stop.png'), '停止', self)
         self.refresh_button = QAction(QIcon('Assets/refresh.png'), '刷新', self)
+        # 地址栏
         self.home_button = QAction(QIcon('Assets/home.png'), '主页', self)
         self.enter_button = QAction(QIcon('Assets/enter.png'), '转到', self)
-        # 其他组件
         self.url_text_bar = QLineEdit()
         self.url_text_bar.setMinimumWidth(300)
+        self.site_icon = QAction(self)
+        # 进度条
         self.progress_bar = QProgressBar()
         self.progress_bar.setMaximumWidth(120)
+        # 设置按钮
+        self.set_button = QAction(QIcon('Assets/setting.png'), '设置', self)
         # 工具条添加组件
         self.navigation_bar.addAction(self.back_button)
         self.navigation_bar.addAction(self.next_button)
         self.navigation_bar.addAction(self.stop_button)
         self.navigation_bar.addAction(self.refresh_button)
-        self.navigation_bar.addSeparator()
         self.navigation_bar.addAction(self.home_button)
         self.navigation_bar.addSeparator()
+        self.navigation_bar.addAction(self.site_icon)
         self.navigation_bar.addWidget(self.url_text_bar)
-        self.navigation_bar.addSeparator()
         self.navigation_bar.addAction(self.enter_button)
         self.navigation_bar.addSeparator()
         self.navigation_bar.addWidget(self.progress_bar)
+        self.navigation_bar.addAction(self.set_button)
         # 事件触发
         self.back_button.triggered.connect(self.browser.back)
         self.next_button.triggered.connect(self.browser.forward)
@@ -76,10 +81,10 @@ class BrowserWindow(QMainWindow):
         self.url_text_bar.setCursorPosition(0)
 
     def renew_title(self, s):
-        self.setWindowTitle(self.name + self.version + " -- " + s)
+        self.setWindowTitle(s + " -- " + self.name)
 
     def renew_icon(self, ico):
-        self.setWindowIcon(ico)
+        self.site_icon.setIcon(ico)
 
     def renew_progress_bar(self, p):
         self.progress_bar.setValue(p)
