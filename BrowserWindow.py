157b5cdba4a23b186529b0e83f9c4ee69788697e
Y.Wang <shengguanglong01@sina.com>
2018-11-28 23:56:09 +0800

    深夜实现多标签功能

--- BrowserWindow.py
+++ BrowserWindow.py
@@ -1,25 +1,48 @@
 from PyQt5.QtCore import QUrl, QSize
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtWebEngineWidgets import QWebEngineView
-from PyQt5.QtWidgets import QMainWindow, QToolBar, QAction, QLineEdit, QProgressBar, QLabel
+from PyQt5.QtWidgets import QToolBar, QAction, QLineEdit, QProgressBar, QLabel, QMainWindow, QTabWidget
 
 import About
 
 
-class BrowserWindow(QMainWindow):
-    name = "PyBrowser Powered by Y.Wang"
-    version = "1.01.20181128"
+class BrowserEngineView(QWebEngineView):
+    tabs = []
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        # 窗体
-        self.setWindowTitle(self.name)
-        self.setWindowIcon(QIcon('Assets/main.png'))
-        self.resize(1200, 800)
+    def __init__(self, Main, parent=None):
+        super(BrowserEngineView, self).__init__(parent)
+        self.mainWindow = Main
+
+    def createWindow(self, QWebEnginePage_WebWindowType):
+        webview = BrowserEngineView(self.mainWindow)
+        tab = BrowserTab(self.mainWindow)
+        tab.browser = webview
+        tab.setCentralWidget(tab.browser)
+        # 事件触发
+        tab.back_button.triggered.connect(tab.browser.back)
+        tab.next_button.triggered.connect(tab.browser.forward)
+        tab.stop_button.triggered.connect(tab.browser.stop)
+        tab.refresh_button.triggered.connect(tab.browser.reload)
+        tab.home_button.triggered.connect(tab.navigate_to_home)
+        tab.enter_button.triggered.connect(tab.navigate_to_url)
+        tab.set_button.triggered.connect(tab.create_about_window)
+        # 触发映射
+        tab.url_text_bar.returnPressed.connect(tab.navigate_to_url)
+        tab.browser.urlChanged.connect(tab.renew_urlbar)
+        tab.browser.loadProgress.connect(tab.renew_progress_bar)
+        self.tabs.append(tab)
+        self.mainWindow.create_new_tab(tab)
+        return webview
+
+
+class BrowserTab(QMainWindow):
+    def __init__(self, Main, parent=None):
+        super(BrowserTab, self).__init__(parent)
+        self.mainWindow = Main
         self.AboutDialog = About.AboutDialog()
         # 浏览器窗体
-        self.browser = QWebEngineView()
-        self.browser.load(QUrl("http://www.hao123.com/"))
+        self.browser = BrowserEngineView(self.mainWindow)
+        self.browser.load(QUrl("about:blank"))
         self.setCentralWidget(self.browser)
         # 工具条
         self.navigation_bar = QToolBar('Navigation')
@@ -38,8 +61,6 @@ class BrowserWindow(QMainWindow):
         self.ssl_label2 = QLabel(self)
         self.url_text_bar = QLineEdit(self)
         self.url_text_bar.setMinimumWidth(300)
-        self.site_icon = QLabel(self)
-        # self.site_icon.setEnabled(False)
         # 进度条
         self.progress_bar = QProgressBar()
         self.progress_bar.setMaximumWidth(120)
@@ -54,7 +75,6 @@ class BrowserWindow(QMainWindow):
         self.navigation_bar.addSeparator()
         self.navigation_bar.addWidget(self.ssl_label1)
         self.navigation_bar.addWidget(self.ssl_label2)
-        self.navigation_bar.addWidget(self.site_icon)
         self.navigation_bar.addWidget(self.url_text_bar)
         self.navigation_bar.addAction(self.enter_button)
         self.navigation_bar.addSeparator()
@@ -71,8 +91,6 @@ class BrowserWindow(QMainWindow):
         # 触发映射
         self.url_text_bar.returnPressed.connect(self.navigate_to_url)
         self.browser.urlChanged.connect(self.renew_urlbar)
-        self.browser.titleChanged.connect(self.renew_title)
-        self.browser.iconChanged.connect(self.renew_icon)
         self.browser.loadProgress.connect(self.renew_progress_bar)
 
     def navigate_to_url(self):
@@ -98,15 +116,46 @@ class BrowserWindow(QMainWindow):
         self.url_text_bar.setText(s.toString())
         self.url_text_bar.setCursorPosition(0)
 
-    def renew_title(self, s):
-        self.setWindowTitle(s + " - " + self.name)
-
-    def renew_icon(self, ico):
-        pixmapx = ico.pixmap(32, 32)
-        self.site_icon.setPixmap(pixmapx)
-
     def renew_progress_bar(self, p):
         self.progress_bar.setValue(p)
 
     def create_about_window(self):
         self.AboutDialog.show()
+
+
+class BrowserWindow(QMainWindow):
+    name = "PyBrowser Powered by Y.Wang"
+    version = "2.0.20181128"
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # 窗体
+        self.setWindowTitle(self.name)
+        self.setWindowIcon(QIcon('Assets/main.png'))
+        self.resize(1200, 800)
+        self.tabs = QTabWidget()
+        self.tabs.setTabsClosable(True)
+        self.tabs.setMovable(True)
+        self.setCentralWidget(self.tabs)
+        self.tabs.tabCloseRequested.connect(self.close_current_tab)
+        self.tabs.tabBarDoubleClicked.connect(self.add_blank_tab)
+        self.add_blank_tab(0)
+
+    def add_blank_tab(self, p):
+        browsertab = BrowserTab(self)
+        i = self.tabs.addTab(browsertab, "空白页")
+        self.tabs.setCurrentIndex(i)
+        browsertab.browser.titleChanged.connect(lambda title: self.tabs.setTabText(i, title))
+        browsertab.browser.iconChanged.connect(lambda icon: self.tabs.setTabIcon(i, icon))
+
+    def create_new_tab(self, tab):
+        i = self.tabs.addTab(tab, "空白页")
+        self.tabs.setCurrentIndex(i)
+        tab.browser.titleChanged.connect(lambda title: self.tabs.setTabText(i, title))
+        tab.browser.iconChanged.connect(lambda icon: self.tabs.setTabIcon(i, icon))
+
+    def close_current_tab(self, i):
+        if self.tabs.count() > 1:
+            self.tabs.removeTab(i)
+        else:
+            self.close()
--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 
-from Browser import BrowserWindow
+from BrowserWindow import BrowserWindow
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
